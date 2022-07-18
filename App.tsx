9273785652112297ec2a402a375e73f7dd778dd5
Mateus Leonardo <m4teusmatos@gmail.com>
2022-07-17 21:36:55 -0300

    alterando a statubs bar

--- App.tsx
+++ App.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { THEME } from './src/styles/theme';
-import { Text, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
+
 import { SignIn } from './src/screens/SignIn/SignIn';
+import { Loading } from './src/components/Loading/Loading';
 
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
   return (
     <>
-      <StatusBar barStyle='default' />
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor="transparent"
+        translucent
+      />
       <NativeBaseProvider theme={THEME}>
-        <SignIn />
+        {fontsLoaded ? <SignIn /> : <Loading />}
       </NativeBaseProvider>
     </>
   );
