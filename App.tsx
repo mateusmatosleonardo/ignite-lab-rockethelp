7a9b665107b8cbf50e089d42dfda49f0d315cc27
Mateus Leonardo <m4teusmatos@gmail.com>
2022-07-17 21:18:45 -0300

    adding styles global the native-base

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { THEME } from './src/styles/theme';
+import { Text, StatusBar } from 'react-native';
+import { SignIn } from './src/screens/SignIn/SignIn';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle='default' />
+      <NativeBaseProvider theme={THEME}>
+        <SignIn />
+      </NativeBaseProvider>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
