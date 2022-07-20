718ae806d63ea1e5ef4f2913f96465b34a1dcb1b
Mateus Leonardo <m4teusmatos@gmail.com>
2022-07-20 12:13:48 -0300

    feat: iniciando integracao com firebase

--- android/app/src/main/java/com/mateusleonardo/rockethelp/MainActivity.java
+++ android/app/src/main/java/com/mateusleonardo/rockethelp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rockethelp;
+package com.mateusleonardo.rockethelp;
 
 import android.os.Build;
 import android.os.Bundle;
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -70,10 +70,10 @@ export function Home() {
           alignItems="center"
         >
           <Heading color="gray.100">
-            Meus chamados
+            Solicitações
           </Heading>
           <Text color="gray.200">
-            3
+            {orders.length}
           </Text>
         </HStack>
         <HStack space={3} mb={8}>
