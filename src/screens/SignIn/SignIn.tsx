11823bbb3c2bfa450570547fac6320a7338c8484
Mateus Leonardo <m4teusmatos@gmail.com>
2022-07-20 14:30:27 -0300

    iniciando integracao com firebase

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,19 +1,46 @@
 import { useState } from 'react';
-
 import { VStack, Heading, Icon, useTheme } from 'native-base';
-
+import auth from '@react-native-firebase/auth';
 import { Envelope, Key } from 'phosphor-react-native';
-
 import Logo from '../../assets/logo_primary.svg';
-
 import { Input } from '../../components/Input/Input';
 import { Button } from '../../components/Button/Button';
+import { Alert } from 'react-native';
 
 export function SignIn() {
-  const [name, setName] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
   const { colors } = useTheme();
+
+  function handleSignIn() {
+    if (!email || !password) {
+      return Alert.alert('Entrar', 'Informe e-mail ou senha inválida.');
+    }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+
+        if (error.code === 'auth/invalid-email') {
+          return Alert.alert('Entrar', 'E-mail inválido');
+        }
+
+        if (error.code === 'auth/wrong-password') {
+          return Alert.alert('Entrar', 'E-mail ou senha inválida.');
+        }
+
+        if (error.code === 'auth/user-not-found') {
+          return Alert.alert('Entrar', 'E-mail ou senha inválida.');
+        }
+
+      });
+  }
 
   return (
     <VStack
@@ -35,7 +62,7 @@ export function SignIn() {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />}
             ml={4} />}
-        onChangeText={setName}
+        onChangeText={setEmail}
       />
       <Input
         mb={8}
@@ -47,7 +74,12 @@ export function SignIn() {
         onChangeText={setPassword}
       />
 
-      <Button title='Entrar' w="full" />
+      <Button
+        title='Entrar'
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
     </VStack>
   )
 }
