c7de94a208e8e14dbb6beed32a75d80fbc8f3204
Mateus Leonardo <m4teusmatos@gmail.com>
2022-07-21 14:48:11 -0300

    feat: iniciando integracao de solucao na screen de details

--- src/screens/Details/Details.tsx
+++ src/screens/Details/Details.tsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react';
 import { useRoute } from '@react-navigation/native';
 import { Text, VStack } from 'native-base';
+import firestore from '@react-native-firebase/firestore';
 import { Header } from '../../components/Header/Header';
-import { RouteParams } from './types';
+import { RouteParams, OrderDetails } from './types';
+import { OrderFirestoreDTO } from '../../DTOs/OrderDTO';
 
 export function Details() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [solution, setSolution] = useState<string>('');
+  const [order, setOrder] = useState<OrderDetails>({} as OrderDetails);
 
   const route = useRoute();
   const { orderId } = route.params as RouteParams;
+
+  useEffect(() => {
+    firestore()
+      .collection<OrderFirestoreDTO>('orders')
+      .doc(orderId)
+      .get()
+      .then(doc => {
+        const { description, patrimony, created_at, closed_at, status, solution } = doc.data();
+      })
+  }, []);
 
   return (
     <VStack
--- src/screens/Details/types.ts
+++ src/screens/Details/types.ts
@@ -1,3 +1,11 @@
+import { OrderProps } from "../../components/Order/types";
+
 export type RouteParams = {
   orderId: string;
+}
+
+export type OrderDetails = OrderProps & {
+  description: string;
+  solution: string;
+  closed: string;
 }
