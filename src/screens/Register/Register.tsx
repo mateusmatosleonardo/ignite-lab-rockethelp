72899650f18363ee41e457bd7882894cbb6c1dd5
Mateus Leonardo <m4teusmatos@gmail.com>
2022-07-21 11:31:10 -0300

    feat: integrando registro de order com o firestore

--- src/screens/Register/Register.tsx
+++ src/screens/Register/Register.tsx
@@ -1,15 +1,51 @@
+import { useState } from 'react';
+import firestore from '@react-native-firebase/firestore';
+import { useNavigation } from '@react-navigation/native';
 import { VStack } from 'native-base';
 import { Button } from '../../components/Button/Button';
 import { Header } from '../../components/Header/Header';
 import { Input } from '../../components/Input/Input';
+import { Alert } from 'react-native';
 
 export function Register() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [patrimony, setPatrimony] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+
+  const navigation = useNavigation();
+
+  function handleNewOrderRegister() {
+    if (!patrimony || !description) {
+      return Alert.alert('Registrar', 'Preencha todos os campos');
+    }
+
+    setIsLoading(true);
+    firestore()
+      .collection('orders')
+      .add({
+        patrimony,
+        description,
+        status: 'open',
+        created_at: firestore.FieldValue.serverTimestamp()
+      })
+      .then(() => {
+        Alert.alert('Solicitação', 'Solicitação registrada com sucesso.');
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+        return Alert.alert('Solicitação', 'Não foi possível registrar o pedido.')
+      });
+  }
+
   return (
     <VStack flex={1} p={1} bg="gray.600">
       <Header title='Nova solicitação' />
       <Input
         placeholder='Número do patrimônio'
         mt={4}
+        onChangeText={setPatrimony}
       />
       <Input
         placeholder='Descrição do problema'
@@ -17,10 +53,13 @@ export function Register() {
         flex={1}
         multiline
         textAlignVertical='top'
+        onChangeText={setDescription}
       />
       <Button
         title='Cadastrar'
         mt={5}
+        isLoading={isLoading}
+        onPress={handleNewOrderRegister}
       />
     </VStack>
   );
